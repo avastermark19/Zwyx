4c2f9163e1dbd6c599bfb93db19ae18e496f67e4
avastermark19 <30867791+avastermark19@users.noreply.github.com>
2019-05-27 12:19:27 -0500

    Add files via upload

--- R/plotMedianReadCounts.R
+++ R/plotMedianReadCounts.R
@@ -1,7 +1,21 @@
+#' @title countMedian Function to calculate median.
+#' @description This function generates "median".
+#' @usage countMedian(scaffolds, Sample, G_RESULT2)
+#' @param scaffolds Choice of scaffold.
+#' @param Sample Choice of sample.
+#' @param G_RESULT2 G_RESULT file.
+#' @details This function returns median expression for selected scaffold and sample.
+#' @return Returns median value.
+#' @examples
+#'
+#' @author AJ Vaestermark, JR Walters.
+#' @references The "Zwyx" package, 2018 (in press).
 
 # The following reads from the object, not the files...
 
-countMedian <- function(scaffolds='DPSCF300001', Sample=1) {
+countMedian <- function(scaffolds='DPSCF300001', Sample=1, G_RESULT2) {
+
+ # data(G.RESULT)
 
   x <- c('Danaus_poolC_Plex_BOS_HI023_M_CTTGGA_L003realigned.bam', 'MK_Plex_Mex_1742_F_GATCAG_L002realigned.bam', 'MK_Plex_NJ_116_M_GAGTGG_L008.realigned.bam',
          'MK_Plex_StMFL_122_F_ATCACG_L008realigned.bam', 'MK_Plex_StMFL_109_M_ACTTGA_L007realigned.bam', 'MK_Plex_TX_T11_F_GGCTAC_L0013realigned.bam')
@@ -12,10 +26,10 @@ countMedian <- function(scaffolds='DPSCF300001', Sample=1) {
   sample2 <- x[Sample]
 
   #G_RESULT[1]@unlistData@elementMetadata@listData$Danaus_poolC_Plex_BOS_HI023_M_CTTGGA_L003realigned.bam
-  G_RESULT[SCAFFOLDS]@unlistData@elementMetadata@listData[Sample]
+  G_RESULT2[SCAFFOLDS]@unlistData@elementMetadata@listData[Sample]
 
   #M3 <- unname( unlist( ( suppressMessages(    getReadCountsFromBAM(x[i], WL=500, refSeqName=scaffolds)))@elementMetadata@listData[1]));
-  M3 <-  unname(unlist(G_RESULT[SCAFFOLDS]@unlistData@elementMetadata@listData[Sample]))
+  M3 <-  unname(unlist(G_RESULT2[SCAFFOLDS]@unlistData@elementMetadata@listData[Sample]))
 
   return( median(M3) )
 
@@ -42,7 +56,23 @@ countMedian <- function(scaffolds='DPSCF300001', Sample=1) {
 
 # The following reads from the object, not the files...
 
-plotMedianReadCounts <- function() {
+#' @title countMedian Function to plot median read counts.
+#' @description This function generates "median" read count plot.
+#' @usage plotMedianReadCounts(G_RESULT3)
+#' @param G_RESULT3 G_RESULT file.
+#' @details This function plots median expression for selected sample.
+#' @return Returns median value plot for samples.
+#' @examples
+#' data(G.RESULT)
+#' plotMedianReadCounts(G_RESULT)
+#' @author AJ Vaestermark, JR Walters.
+#' @references The "Zwyx" package, 2018 (in press).
+
+
+plotMedianReadCounts <- function(G_RESULT3) {
+
+  requireNamespace("txtplot")
+  requireNamespace("stringr")
 
   yyx<-NULL
   for(i in 1:5397) {
@@ -53,23 +83,24 @@ plotMedianReadCounts <- function() {
 
   pb <- txtProgressBar(max=6)
 
+  SAMPLE <-  sample(1:5397, 30) ;
 xHI023_M <- NULL
-for(i in 1:5397) xHI023_M <- c( xHI023_M , countMedian(yyx[i], Sample=1) )
+for(i in 1:30) xHI023_M <- c( xHI023_M , countMedian(yyx[SAMPLE[i]], Sample=1, G_RESULT3) )
 setTxtProgressBar(pb, 1)
 x1742_F <- NULL
-for(i in 1:5397) x1742_F <- c( x1742_F , countMedian(yyx[i], Sample=2) )
+for(i in 1:30) x1742_F <- c( x1742_F , countMedian(yyx[SAMPLE[i]], Sample=2, G_RESULT3) )
 setTxtProgressBar(pb, 2)
 x116_M <- NULL
-for(i in 1:5397) x116_M <- c( x116_M , countMedian(yyx[i], Sample=3) )
+for(i in 1:30) x116_M <- c( x116_M , countMedian(yyx[SAMPLE[i]], Sample=3, G_RESULT3) )
 setTxtProgressBar(pb, 3)
 x122_F <- NULL
-for(i in 1:5397) x122_F <- c( x122_F , countMedian(yyx[i], Sample=4) )
+for(i in 1:30) x122_F <- c( x122_F , countMedian(yyx[SAMPLE[i]], Sample=4, G_RESULT3) )
 setTxtProgressBar(pb, 4)
 x109_M <- NULL
-for(i in 1:5397) x109_M <- c( x109_M , countMedian(yyx[i], Sample=5) )
+for(i in 1:30) x109_M <- c( x109_M , countMedian(yyx[SAMPLE[i]], Sample=5, G_RESULT3) )
 setTxtProgressBar(pb, 5)
 xT11_F <- NULL
-for(i in 1:5397) xT11_F <- c( xT11_F , countMedian(yyx[i], Sample=6) )
+for(i in 1:30) xT11_F <- c( xT11_F , countMedian(yyx[SAMPLE[i]], Sample=6, G_RESULT3) )
 setTxtProgressBar(pb, 6)
 
 close(pb)
